3ebe94e5e1c371463b5e4534e8c1e47e7ae16634
Oleg Sobolev <iiiyxep@list.ru>
2023-03-05 13:50:54 +0100

    logger middleware

--- product-service/src/libs/lambda.ts
+++ product-service/src/libs/lambda.ts
@@ -2,7 +2,12 @@ import middy from "@middy/core";
 import cors from "@middy/http-cors";
 import errorHandler from "src/middlewares/error-handler";
 import jsonEncoder from "src/middlewares/json-encoder";
+import logger from "src/middlewares/logger";
 
 export const middyfy = (handler) => {
-  return middy(handler).use(cors()).use(jsonEncoder()).use(errorHandler());
+  return middy(handler)
+    .use(cors())
+    .use(jsonEncoder())
+    .use(errorHandler())
+    .use(logger());
 };
